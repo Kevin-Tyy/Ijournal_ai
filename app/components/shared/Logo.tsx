91f9763aaf013d64305bd67271b224d5d70ca65a
Kevin TUYIZERE <122741116+Kevin-Tyy@users.noreply.github.com>
2024-04-23 10:53:03 +0200

    chore: udpated metadata

--- app/components/shared/Logo.tsx
+++ app/components/shared/Logo.tsx
@@ -3,11 +3,18 @@ import Link from "next/link";
 import React from "react";
 interface Props {
   isLight?: boolean;
+  isMobile?: boolean;
 }
-export default function Logo({ isLight }: Props) {
+export default function Logo({ isLight, isMobile }: Props) {
   return (
-    <Link href="/" className={``}>
-      <Image src="/logo/Journal Ai.svg" alt="ijournal" height={80} width={80} className={`w-44 max-h-16 min-w-44`} />
+    <Link href="/">
+      <Image
+        src="/logo/Journal Ai.svg"
+        alt="imaginate"
+        height={80}
+        width={80}
+        className={`w-44 max-h-16 min-w-44 ${!isLight && "w-32 min-w-32"} ${isMobile && "w-28 min-w-28"}`}
+      />
     </Link>
   );
 }
